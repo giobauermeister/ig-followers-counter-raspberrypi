f0e05ad0fb156c4c1d99d14acc0f7541ac0940d8
Giovanni Bauermeister <giovanni.bauermeister@gmail.com>
2017-05-24 23:03:29 -0300

    added flipcounter

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var api = require('instagram-node').instagram();
 var bodyParser = require('body-parser');
 var server = require('http').Server(app);
 var request = require('request');
-//var io = require('socket.io')(server);
+var io = require('socket.io')(server);
 var path = require('path');
 var bodyParser = require('body-parser');
-var io = require('socket.io')(server);
 
 app.use(bodyParser.json())
 //request.use(bodyParser.json())
@@ -65,27 +64,18 @@ app.get('/handleauth', function(req, res) {
 // https://api.instagram.com/v1/users/223155900/?access_token=223155900.9d3d02c.cdc226e359154865ae39885c145718db
 function make_get() {
   request.get(ig_api_url + USER_ID + '/?access_token=' + TOKEN, function(err, res, body) {
-    console.log(err);
-    console.log(res);
+    //console.log(err);
+    //console.log(res);
     var response = JSON.parse(body);
     console.log("Username: " + response.data.username);
     console.log("Name: " + response.data.full_name);
     console.log("Followers: " + response.data.counts.followed_by);
     io.emit('responseEvent', response);
-    setTimeout(make_get, 10);
+    setTimeout(make_get, 3000);
     //console.log("User: " + body.data);
     //console.log("Followers: " + result.data.followed_by);
   });
 }
-// request.get('ig_api_url + USER_ID + "/?access_token=" + TOKEN', function(err, result, body) {
-//   console.log("User: " + result.data.username);
-//   console.log("Followers: " + result.data.followed_by);
-// });
-
-// app.get(ig_api_url + USER_ID + '/?access_token=' + TOKEN, function(err, result) {
-//   console.log("User: " + result.data.username);
-//   console.log("Followers: " + result.data.followed_by);
-// });
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -96,84 +86,3 @@ server.listen(8080, function() {
  var port = server.address().port
  console.log("Server listening on %s:%s...", host, port);
 });
-
-//app.use(express.static(path.join(__dirname, '/public')));
-
-// const CLIENTID = '9d3d02c6cf2c4e64abaec4605ec06eba'
-// //const REDIRECTURI = 'http://www.filipeflop.com'
-// const REDIRECTURI = 'http://localhost:8080'
-// const USER = 'giobauermeister'
-// const PASS = 'ul9mgz538x'
-
-// request.post(
-//     'http://www.google.com/',
-//     { json: { key: 'value' } },
-//     function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             console.log(body)
-//         }
-//     }
-// );
-
-
-
-//URL https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code
-
-// var return_uri
-
-// var redirect = request.get('https://api.instagram.com/oauth/authorize/?client_id='+CLIENTID+'&redirect_uri='+REDIRECTURI+'&response_type=code',
-//                 function(err, res, body) {
-//           //console.log(res);
-//           console.log(this.uri.href);
-//           return_uri = this.uri.href;
-// });
-
-// var redirect = request.get('https://api.instagram.com/oauth/authorize/?client_id='+CLIENTID+'&redirect_uri='+REDIRECTURI+'&response_type=token',
-//                 function(err, res, body) {
-//           //console.log(res);
-//           console.log(this.uri.href);
-//           return_uri = this.uri.href;
-// });
-//
-// app.get('/', function (req, res) {
-//    res.redirect(return_uri)
-//    console.log(req);
-// });
-
-// var data = {
-//   username: USER,
-//   password: PASS
-// }
-//
-// var return_token = request.post(return_uri, data, function(err,res,body) {
-//   console.log(body);
-//   //console.log(this.uri.href)
-// });
-
-
-// request.get('http://ip-api.com/json/?fields=8405', function(err, res, body) {
-//     console.log(body);
-// });
-
-//var locationData;
-
-//server.listen(3000, '192.168.0.24', function() {
-// var host = server.address().address
-// var port = server.address().port
-// console.log("Server listening on %s:%s...", host, port);
-//});
-
-// server.listen(8080, function() {
-//  var host = server.address().address
-//  var port = server.address().port
-//  console.log("Server listening on %s:%s...", host, port);
-// });
-
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// app.post('/location', function(req, res) {
-//  locationData = req.body
-//  io.emit('locationEvent', locationData);
-// });
