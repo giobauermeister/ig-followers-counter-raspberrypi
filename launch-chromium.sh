afc220cdf5dab00bc0e16f9b9fffc108ee15733e
Giovanni Bauermeister <giovanni.bauermeister@gmail.com>
2017-06-12 16:28:34 -0300

    minor changes

--- launch-chromium.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chromium-browser --incognito --window-size=800,480 --kiosk "http://igcounter.local:8080/authorize_user" &
-
--- launch-ig-counter.sh
+++ launch-ig-counter.sh
@@ -2,8 +2,8 @@
 
 while true
 do
-	forever start /home/pi/ig-followers-counter/app.js
+	forever start /home/pi/ig-followers-counter-raspberrypi/app.js
 	sleep 3
 	export DISPLAY=:0
-	/usr/bin/chromium-browser --incognito --window-size=800,480 --kiosk "http://igcounter.local:8080/authorize_user"
+	/usr/bin/chromium-browser --incognito --window-size=800,480 --kiosk "http://raspberrypi.local:8080/authorize_user"
 done
