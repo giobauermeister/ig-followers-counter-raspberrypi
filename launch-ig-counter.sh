daf756e16f8a78192e27d9f5ef3744af70195de7
Giovanni Bauermeister <giovanni.bauermeister@gmail.com>
2017-06-14 09:36:38 -0300

    changed chromium address to localhost

--- launch-ig-counter.sh
+++ launch-ig-counter.sh
@@ -5,5 +5,5 @@ do
 	forever start /home/pi/ig-followers-counter-raspberrypi/app.js
 	sleep 3
 	export DISPLAY=:0
-	/usr/bin/chromium-browser --incognito --window-size=800,480 --kiosk "http://raspberrypi.local:8080/authorize_user"
+	/usr/bin/chromium-browser --incognito --window-size=800,480 --kiosk "http://localhost:8080/authorize_user"
 done
